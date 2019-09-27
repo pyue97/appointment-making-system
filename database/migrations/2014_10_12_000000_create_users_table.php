3420e48c18012cc42310fa6d999d6be720ea47a6
Jennifer <pyue97@gmail.com>
2019-09-27 11:16:33 +0800

    npm

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('username');
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('usertype');
             $table->rememberToken();
             $table->timestamps();
         });
